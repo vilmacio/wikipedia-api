8a905a14d52e23d8a3b5accf9610c1eddc559f0b
vilmacio <vilmacio@users.noreply.github.com>
2020-11-22 13:55:19 -0300

    test: ensures main instance is a function

--- test/wikipedia-api.spec.ts
+++ test/wikipedia-api.spec.ts
@@ -3,7 +3,12 @@ import { MissingParamError } from '../src/errors/missing-param'
 
 describe('wikipedia-api', () => {
   describe('object instance', () => {
-    test('should throws MissingParamError if url is not provided', async () => {
+    test('should be a function', () => {
+      expect(wikipedia).toBeTruthy()
+      expect(typeof wikipedia).toBe('function')
+    })
+
+    test('should throws MissingParamError if url is not provided', () => {
       expect(() => { wikipedia('') }).toThrow(new MissingParamError('url'))
     })
   })
