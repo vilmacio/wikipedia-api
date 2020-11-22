ae3d628ba573262558848f5c8f466aa5a6e800f9
vilmacio <vilmacio@users.noreply.github.com>
2020-11-22 15:05:27 -0300

    test: ensures main instance throws MissingParamError if article is not provided

--- test/wikipedia-api.spec.ts
+++ test/wikipedia-api.spec.ts
@@ -1,21 +1,36 @@
 import wikipedia from '../src/wikipedia-api'
 import { MissingParamError } from '../src/errors/missing-param'
 import { InvalidParamError } from '../src/errors/invalid-param'
+import { ArticleAttributes } from '../src/protocols/article'
 
-describe('wikipedia-api', () => {
-  describe('object instance', () => {
+describe('wikipedia-api [instance]', () => {
+  describe('when imported', () => {
     test('should be a function', () => {
       expect(wikipedia).toBeTruthy()
       expect(typeof wikipedia).toBe('function')
     })
+  })
 
+  describe('when invoked with string parameter', () => {
     test('should throws MissingParamError if url is not provided', () => {
       expect(() => { wikipedia('') }).toThrow(new MissingParamError('url'))
     })
 
     test('should throws InvalidParamError if wikipedia url is invalid', () => {
       expect(() => { wikipedia('invalid_url') })
-        .toThrow(new InvalidParamError('url', 'The url parameter must be a string'))
+        .toThrow(new InvalidParamError('url', 'The provided wikipedia url is invalid'))
+    })
+  })
+
+  describe('when invoked with object parameter', () => {
+    test('should throws MissingParamError if article is not provided', () => {
+      const attributes:ArticleAttributes = {
+        article: null,
+        lang: 'any_lang'
+      }
+
+      expect(() => { wikipedia(attributes) })
+        .toThrow(new MissingParamError('article'))
     })
   })
 })
