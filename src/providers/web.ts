9c014757d330c52343de3a0c3a5dea919ac280e6
vilmacio <vilmacio@users.noreply.github.com>
2020-11-29 12:29:30 -0300

    feat: ensures web provider throw ServiceError if fetch throws

--- src/providers/web.ts
+++ src/providers/web.ts
@@ -1,16 +1,13 @@
-import request from 'request'
+import fetch from 'node-fetch'
 import ServiceError from '../errors/service-error'
 
-export const articlePage = async (article:string, lang: string):Promise<any> => {
-  return new Promise((resolve, reject) => {
-    request(`https://${lang}.wikipedia.org/wiki/${article}`, {
-      method: 'GET'
-    }, (err, result) => {
-      if (err) {
-        throw new ServiceError('The connection failed. Try to set a correct language.')
-      }
-      if (result.statusCode === 404) resolve(null)
-      if (result.statusCode === 200) resolve(result.body)
-    })
-  })
+export const articlePage = async (article:string, lang: string):Promise<string> => {
+  try {
+    const url = `https://${lang}.wikipedia.org/wiki/${article}`
+    const response = await fetch(url)
+    if (response.status === 200) return await response.text()
+    if (response.status === 404) return undefined
+  } catch (error) {
+    throw new ServiceError('The connection failed. Try to set a correct language.')
+  }
 }
