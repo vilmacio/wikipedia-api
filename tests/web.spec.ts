edc5c0b494adac3142f9b1937f289b196ed01103
vilmacio <vilmacio@users.noreply.github.com>
2020-11-29 17:00:10 -0300

    refactor: test suites

--- tests/web.spec.ts
+++ tests/web.spec.ts
@@ -1,29 +1,11 @@
-import { articlePage } from '../src/providers/web'
 import nock from 'nock'
 import * as fetch from 'node-fetch'
+
+import { articlePage } from '../src/providers/web'
 import ServiceError from '../src/errors/service-error'
 
 describe('Web Provider', () => {
   const wikipediaURL = 'https://en.wikipedia.org/wiki/any_article'
-
-  test('should return content if article is found', async () => {
-    nock('https://en.wikipedia.org/wiki')
-      .get(/\/.*/)
-      .reply(200, '<html>content</html>')
-
-    const response = await articlePage(wikipediaURL)
-    expect(typeof response).toBe('string')
-    expect(response).toBe('<html>content</html>')
-  })
-
-  test('should return undefined if article is not found', async () => {
-    nock('https://en.wikipedia.org/wiki')
-      .get(/\/.*/)
-      .reply(404, '<html>content</html>')
-
-    const response = await articlePage(wikipediaURL)
-    expect(response).toBeUndefined()
-  })
 
   test('should throw ServiceError if fetch throws', async () => {
     jest.spyOn(fetch, 'default').mockImplementationOnce(() => { throw new Error() })
@@ -40,5 +22,24 @@ describe('Web Provider', () => {
     const fetchSpy = jest.spyOn(fetch, 'default')
     await articlePage(wikipediaURL)
     expect(fetchSpy).toHaveBeenCalledWith(wikipediaURL)
+  })
+
+  test('should return undefined if article is not found', async () => {
+    nock('https://en.wikipedia.org/wiki')
+      .get(/\/.*/)
+      .reply(404, '<html>content</html>')
+
+    const response = await articlePage(wikipediaURL)
+    expect(response).toBeUndefined()
+  })
+
+  test('should return content if article is found', async () => {
+    nock('https://en.wikipedia.org/wiki')
+      .get(/\/.*/)
+      .reply(200, '<html>content</html>')
+
+    const response = await articlePage(wikipediaURL)
+    expect(typeof response).toBe('string')
+    expect(response).toBe('<html>content</html>')
   })
 })
--- tests/wikipedia-api.spec.ts
+++ tests/wikipedia-api.spec.ts
@@ -1,98 +1,99 @@
+import nock from 'nock'
+
 import wikipedia from '../src/wikipedia-api'
 import * as webProvider from '../src/providers/web'
 import { MissingParamError } from '../src/errors/missing-param'
 import { InvalidParamError } from '../src/errors/invalid-param'
 import { ArticleAttributes } from '../src/protocols/article'
-import nock from 'nock'
 
-describe('wikipedia-api [instance]', () => {
-  afterAll(() => {
-    nock(/^https?:\/\/..\.wikipedia.org\/wiki\//)
-      .get(/../)
-      .reply(200, {
-        inner: '<html>content</html>'
+describe('WIkipedia API', () => {
+  beforeAll(() => {
+    nock('https://en.wikipedia.org/wiki')
+      .persist()
+      .get(/\/.*/)
+      .reply(200, '<html>content</html>')
+  })
+
+  describe('default', () => {
+    describe('when imported', () => {
+      test('should be a function', () => {
+        expect(wikipedia).toBeTruthy()
+        expect(typeof wikipedia).toBe('function')
       })
-  })
+    })
 
-  describe('when imported', () => {
-    test('should be a function', () => {
-      expect(wikipedia).toBeTruthy()
-      expect(typeof wikipedia).toBe('function')
+    describe('when invoked with string parameter', () => {
+      test('should throws MissingParamError if url is not provided', () => {
+        expect(() => { wikipedia('') }).toThrow(new MissingParamError('url'))
+      })
+
+      test('should throws InvalidParamError if wikipedia url is invalid', () => {
+        expect(() => { wikipedia('invalid_url') })
+          .toThrow(new InvalidParamError('url', 'The provided wikipedia url is invalid'))
+      })
+
+      test('should return a object on success', () => {
+        expect(typeof wikipedia('http://en.wikipedia.org/wiki/any_parameter'))
+          .toBe('object')
+      })
+    })
+
+    describe('when invoked with object parameter', () => {
+      test('should throws MissingParamError if article is not provided', () => {
+        const attributes:ArticleAttributes = {
+          article: null,
+          lang: 'any_lang'
+        }
+
+        expect(() => { wikipedia(attributes) })
+          .toThrow(new MissingParamError('article'))
+      })
+
+      test('should throws MissingParamError if lang is not provided', () => {
+        const attributes:ArticleAttributes = {
+          article: 'any_article',
+          lang: null
+        }
+
+        expect(() => { wikipedia(attributes) })
+          .toThrow(new MissingParamError('lang'))
+      })
+
+      test('should return a object on success', () => {
+        const attributes:ArticleAttributes = {
+          article: 'any_article',
+          lang: 'any_lang'
+        }
+
+        expect(typeof wikipedia(attributes))
+          .toBe('object')
+      })
     })
   })
 
-  describe('when invoked with string parameter', () => {
-    test('should throws MissingParamError if url is not provided', () => {
-      expect(() => { wikipedia('') }).toThrow(new MissingParamError('url'))
+  describe('innerHTML()', () => {
+    const wikipediaURL = 'http://en.wikipedia.org/wiki/any_article'
+    const articleAttributes = {
+      article: 'any_article',
+      lang: 'en'
+    }
+
+    test('should call web provider with correct value', async () => {
+      const articlePageSpy = jest.spyOn(webProvider, 'articlePage')
+      await wikipedia(wikipediaURL).innerHTML()
+      await wikipedia(articleAttributes).innerHTML()
+      expect(articlePageSpy).toHaveBeenCalledTimes(2)
+      expect(articlePageSpy).toHaveBeenCalledWith(wikipediaURL)
     })
 
-    test('should throws InvalidParamError if wikipedia url is invalid', () => {
-      expect(() => { wikipedia('invalid_url') })
-        .toThrow(new InvalidParamError('url', 'The provided wikipedia url is invalid'))
-    })
-
-    test('should return a object on success', () => {
-      expect(typeof wikipedia('http://en.wikipedia.org/wiki/any_parameter'))
-        .toBe('object')
-    })
-  })
-
-  describe('when invoked with object parameter', () => {
-    test('should throws MissingParamError if article is not provided', () => {
-      const attributes:ArticleAttributes = {
-        article: null,
-        lang: 'any_lang'
-      }
-
-      expect(() => { wikipedia(attributes) })
-        .toThrow(new MissingParamError('article'))
-    })
-
-    test('should throws MissingParamError if lang is not provided', () => {
-      const attributes:ArticleAttributes = {
-        article: 'any_article',
-        lang: null
-      }
-
-      expect(() => { wikipedia(attributes) })
-        .toThrow(new MissingParamError('lang'))
-    })
-
-    test('should return a object on success', () => {
-      const attributes:ArticleAttributes = {
-        article: 'any_article',
-        lang: 'any_lang'
-      }
-
-      expect(typeof wikipedia(attributes))
-        .toBe('object')
+    test('should return a string on success', async () => {
+      const result = await wikipedia(wikipediaURL).innerHTML()
+      expect(typeof result).toBe('string')
+      expect(result).toBe('<html>content</html>')
     })
   })
-})
 
-describe('innerHTML()', () => {
-  nock('https://en.wikipedia.org/wiki')
-    .persist()
-    .get(/\/.*/)
-    .reply(200, '<html>content</html>')
-
-  const wikipediaURL = 'http://en.wikipedia.org/wiki/any_article'
-  const articleAttributes = {
-    article: 'any_article',
-    lang: 'en'
-  }
-
-  test('should call web provider with correct value', async () => {
-    const articlePageSpy = jest.spyOn(webProvider, 'articlePage')
-    await wikipedia(wikipediaURL).innerHTML()
-    await wikipedia(articleAttributes).innerHTML()
-    expect(articlePageSpy).toHaveBeenCalledTimes(2)
-    expect(articlePageSpy).toHaveBeenCalledWith(wikipediaURL)
-  })
-
-  test('should return a string on success', async () => {
-    const result = await wikipedia(wikipediaURL).innerHTML()
-    expect(typeof result).toBe('string')
-    expect(result).toBe('<html>content</html>')
+  afterAll(() => {
+    nock.isDone()
   })
 })
