deae922ed0bff8ca318883be8374f4f5cb4b4522
vilmacio <vilmacio@users.noreply.github.com>
2020-12-02 15:16:25 -0300

    test: ensures web wikipedia throws if network connection is disabled

--- tests/e2e/providers.test.ts
+++ tests/e2e/providers.test.ts
@@ -1,3 +1,4 @@
+import nock from 'nock'
 import fetch from 'node-fetch'
 
 describe('Providers - E2E tests', () => {
@@ -5,5 +6,18 @@ describe('Providers - E2E tests', () => {
     const response = await fetch('https://en.wikipedia.org/wiki')
     expect(response.status).toBe(200)
     await expect(response.text()).resolves.not.toBe('<html>content</html>')
+  })
+
+  describe('Web Wikipedia', () => {
+    test('should throw if network connection is disabled', async () => {
+      nock.disableNetConnect()
+      const promise = fetch('https://en.wikipedia.org/wiki')
+      await expect(promise).rejects.toThrow()
+      nock.cleanAll()
+    })
+  })
+
+  afterAll(() => {
+    nock.cleanAll()
   })
 })
